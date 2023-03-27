9817408e59585372d53e91f1f1945e371fe7233a
KylieLAnglin <kylie.anglin@uconn.edu>
2023-03-27 16:50:54 -0400

    tree full sample

---
+++ 01_tree_full_sample.py
@@ -0,0 +1,88 @@
+# %%
+# %%
+from re import sub
+import pandas as pd
+import numpy as np
+from library import start
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+import matplotlib.pyplot as plt
+from sklearn.tree import plot_tree
+from scipy import stats
+from sklearn.metrics import mean_squared_error
+
+
+# %%
+FILE_NAME = "feedback_analysis_withpre_post_survey_wide.dta"
+SEED = 6
+PREDICTORS = ["dass_total_p", "neo_n_p", "tses_is_p", "treat", "score0", "score1"]
+
+# %%
+df = pd.read_stata(
+    start.RAW_DATA_DIR + FILE_NAME,
+)
+df["treat"] = df["treat"].map({"No Coaching": 0, "Coaching": 1})
+df["growth"] = df.score2 - df.score1
+
+df["das_stress_p"] = df.das_stress.rank(pct=True)
+df["neo_n_p"] = df.neo_n.rank(pct=True)
+df["neo_e_p"] = df.neo_e.rank(pct=True)
+df["tses_is_p"] = df.tses_is.rank(pct=True)
+df["dass_total_p"] = df.dass_total.rank(pct=True)
+df["tses_total_p"] = df.tses_total.rank(pct=True)
+df = df.dropna(subset=PREDICTORS)
+df = df.dropna(subset=["score2"])
+df = df.dropna(subset=["score1"])
+
+df = df.set_index("id")
+# %%
+# PREDICTORS = [
+#     "das_stress",
+#     "das_depression",
+#     "das_anxiety",
+#     "dass_total",
+#     "neo_n",
+#     "neo_e",
+#     "neo_a",
+#     "neo_c",
+#     "neo_o",
+#     "tses_is",
+#     "tses_cm",
+#     "tses_se",
+#     "tses_total",
+#     "treat",
+#     "score0",
+#     "score1",
+# ]
+y = df.growth
+X = df[PREDICTORS]
+
+
+X_train = X
+y_train = y
+
+# %%
+MIN_SAMPLES = 8
+MIN_SAMPLES = 10 # good
+MIN_SAMPLES = 12 # Victoria good but now missing high treat
+MIN_SAMPLES = 20 #nice and simple
+model = DecisionTreeRegressor(min_samples_leaf=MIN_SAMPLES)
+# model = DecisionTreeRegressor(max_depth=4)
+
+# model = DecisionTreeRegressor()
+model.fit(X_train, y_train)
+
+# %%
+plt.figure(figsize=(10, 8), dpi=150)
+plot_tree(model, feature_names=X.columns, node_ids=True, max_depth = 5)
+# plt.savefig(start.MAIN_DIR + "results/tree.pdf")
+
+# %%
+print("Kathleen:", model.apply(X_train.loc[43].values.reshape(1, -1)))
+print("Victoria:", model.apply(X_train.loc[105].values.reshape(1, -1)))
+print("Liz:", model.apply(X_train.loc[41].values.reshape(1, -1)))
+print("Alex:", model.apply(X_train.loc[57].values.reshape(1, -1)))
+
+# %%
