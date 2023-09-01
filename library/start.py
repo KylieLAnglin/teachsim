a787f71067dfc0e509e730ce88bf90349ed8655e
KylieLAnglin <kylie.anglin@uconn.edu>
2023-09-01 11:18:30 -0400

    temp2

--- library/start.py
+++
@@ -1,7 +0,0 @@
-HOME_DIR = "/Users/kla21002/"
-
-MAIN_DIR = HOME_DIR + "Dropbox/Active/Research/teachsim/"
-# MAIN_DIR = "/Users/kylieanglin/Dropbox/Active/teachsim/"
-
-
-RAW_DATA_DIR = HOME_DIR + "Box Sync/Project SimTeacher/Feedback Analysis (ew3kp@Virginia.EDU)/Output/For Analysis/"
---
+++ start.py
@@ -0,0 +1,7 @@
+HOME_DIR = "/Users/kla21002/"
+MAIN_DIR = HOME_DIR + "Dropbox/Active/Research/teachsim/"
+RAW_DATA_DIR = (
+    HOME_DIR
+    + "Box Sync/Project SimTeacher/Feedback Analysis (ew3kp@Virginia.EDU)/Output/For Analysis/"
+)
+SEED = 14
